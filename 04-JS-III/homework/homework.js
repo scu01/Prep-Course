33502089cd1f8b053bff6b3e1710a2f64a7ca301
scu01 <alexisscuderi01@gmail.com>
2022-02-21 16:33:48 -0300

    homework realizada

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -113,7 +113,6 @@ function multiplicarArgumentos() {
   // Escribe tu código aquí:
   let producto = arguments[0];
   if (arguments.length === 0) return 0;
-  if (arguments.length === 1) return arguments[0];
   for (let i = 1; i < arguments.length; i++) {
     producto *= arguments[i];
   } return producto;
